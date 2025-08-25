26870a457421505ec4d4d1e4c96e0425f115aa7d
jungwoo choi <cjw246@naver.com>
2025-08-25 14:33:00 +0900

    s

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/test-app/',  // GitHub Pages에서 사용할 경로
+  base: '/test/',  // GitHub Pages에서 사용할 경로
   build: {
     outDir: 'dist',  // 빌드 결과물 디렉토리
   },
